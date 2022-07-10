bddfe35f897e4711d2b0a578905ec049289a49ee
Andrey Shaporenko <shaporenkoae@gmail.com>
2022-07-10 15:00:01 +0300

    Add dzt2-3

--- HelloPython/DZ_task2.py
+++ HelloPython/DZ_task2.py
@@ -1,2 +1,12 @@
 #2- Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            result = not x and not y and not z
+            result2 = not (x or y or z)
+            print(x,y,z,'  ', result, '   ', result2)
+if result == result2:
+    print('равенство истинно')
+else:
+    print ('равенство ложно')
